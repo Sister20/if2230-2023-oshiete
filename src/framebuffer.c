5fa6e63951d87f15676a8e7b003d15f547c1da84
chaerla <chelgchen@gmail.com>
2023-02-20 23:51:29 +0800

    feat: setup for 3.7

--- src/framebuffer.c
+++ src/framebuffer.c
@@ -3,14 +3,27 @@
 #include "lib-header/stdmem.h"
 #include "lib-header/portio.h"
 
-void framebuffer_set_cursor(uint8_t r, uint8_t c) {
-    // TODO : Implement
-}
+static const size_t VGA_WIDTH = 80;                            // width
+static const size_t VGA_HEIGHT = 25;                           // height
+static uint16_t *framebuffer = (uint16_t *)MEMORY_FRAMEBUFFER; // type cast to uint16
 
-void framebuffer_write(uint8_t row, uint8_t col, char c, uint8_t fg, uint8_t bg) {
-    // TODO : Implement
-}
+// static size_t framebuffer_row;
+// static size_t framebuffer_col;
+// static uint8_t framebuffer_color;
 
-void framebuffer_clear(void) {
+// void framebuffer_set_cursor(uint8_t r, uint8_t c)
+// {
+//     // TODO : Implement
+// }
+
+// void framebuffer_write(uint8_t row, uint8_t col, char c, uint8_t fg, uint8_t bg)
+// {
+//     // TODO : Implement
+// }
+
+
+void framebuffer_clear(void)
+{
     // TODO : Implement
+    memset(framebuffer, framebuffer_set_color(VGA_COLOR_WHITE, VGA_COLOR_BLACK), VGA_WIDTH * VGA_HEIGHT * sizeof(char));
 }
--- src/lib-header/framebuffer.h
+++ src/lib-header/framebuffer.h
@@ -3,9 +3,9 @@
 
 #include "lib-header/stdtype.h"
 
-#define MEMORY_FRAMEBUFFER (uint8_t *) 0xB8000
-#define CURSOR_PORT_CMD    0x03D4
-#define CURSOR_PORT_DATA   0x03D5
+#define MEMORY_FRAMEBUFFER (uint8_t *)0xB8000
+#define CURSOR_PORT_CMD 0x03D4
+#define CURSOR_PORT_DATA 0x03D5
 
 /**
  * Terminal framebuffer
@@ -13,7 +13,7 @@
  * Starting at MEMORY_FRAMEBUFFER,
  * - Even number memory: Character, 8-bit
  * - Odd number memory:  Character color lower 4-bit, Background color upper 4-bit
-*/
+ */
 
 /**
  * Set framebuffer character and color with corresponding parameter values.
@@ -29,17 +29,42 @@ void framebuffer_write(uint8_t row, uint8_t col, char c, uint8_t fg, uint8_t bg)
 
 /**
  * Set cursor to specified location. Row and column starts from 0
- * 
+ *
  * @param r row
  * @param c column
-*/
+ */
 void framebuffer_set_cursor(uint8_t r, uint8_t c);
 
-/** 
+/**
  * Set all cell in framebuffer character to 0x00 (empty character)
  * and color to 0x07 (gray character & black background)
- * 
+ *
  */
 void framebuffer_clear(void);
+
+enum vga_color
+{
+    VGA_COLOR_BLACK = 0,
+    VGA_COLOR_BLUE = 1,
+    VGA_COLOR_GREEN = 2,
+    VGA_COLOR_CYAN = 3,
+    VGA_COLOR_RED = 4,
+    VGA_COLOR_MAGENTA = 5,
+    VGA_COLOR_BROWN = 6,
+    VGA_COLOR_LIGHT_GREY = 7,
+    VGA_COLOR_DARK_GREY = 8,
+    VGA_COLOR_LIGHT_BLUE = 9,
+    VGA_COLOR_LIGHT_GREEN = 10,
+    VGA_COLOR_LIGHT_CYAN = 11,
+    VGA_COLOR_LIGHT_RED = 12,
+    VGA_COLOR_LIGHT_MAGENTA = 13,
+    VGA_COLOR_LIGHT_BROWN = 14,
+    VGA_COLOR_WHITE = 15,
+};
+
+uint8_t framebuffer_set_color(enum vga_color fg, enum vga_color bg)
+{
+    return fg | bg << 4;
+};
 
 #endif
