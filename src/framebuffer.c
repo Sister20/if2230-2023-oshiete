26740474c3b853215e9212264c851c66b366b61c
Jeffrey Chow <chowprofessional@gmail.com>
2023-02-20 22:51:03 +0700

    feat: 3.7 makefile

--- src/framebuffer.c
+++ src/framebuffer.c
@@ -3,14 +3,14 @@
 #include "lib-header/stdmem.h"
 #include "lib-header/portio.h"
 
-void framebuffer_set_cursor(uint8_t r, uint8_t c) {
-    // TODO : Implement
-}
+// void framebuffer_set_cursor(uint8_t r, uint8_t c) {
+//     // TODO : Implement
+// }
 
-void framebuffer_write(uint8_t row, uint8_t col, char c, uint8_t fg, uint8_t bg) {
-    // TODO : Implement
-}
+// void framebuffer_write(uint8_t row, uint8_t col, char c, uint8_t fg, uint8_t bg) {
+//     // TODO : Implement
+// }
 
-void framebuffer_clear(void) {
-    // TODO : Implement
-}
+// void framebuffer_clear(void) {
+//     // TODO : Implement
+// }
