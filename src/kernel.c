9c645fff3079420cb013a2cfb6922cc15910eba0
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2023-02-27 21:03:34 +0700

    fix: coloring

--- src/kernel.c
+++ src/kernel.c
@@ -14,7 +14,7 @@ void kernel_setup(void)
     /* TO DO : BUAT SPLASH SCREEN */
     splash();
 
-    framebuffer_write(3, 8, 'H', VGA_COLOR_BLUE, VGA_COLOR_CYAN);
+    framebuffer_write(3, 8, 'H', 0, 0xF);
     framebuffer_write(3, 9, 'a', 0, 0xF);
     framebuffer_write(3, 10, 'i', 0, 0xF);
     framebuffer_write(3, 11, '!', 0, 0xF);
