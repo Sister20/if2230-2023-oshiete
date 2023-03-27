5a7d6b77c5a6225ae6fd4f448647f7d19613ca31
chaerla <chelgchen@gmail.com>
2023-03-27 15:03:32 +0800

    fix: sector 0 = cluster 0, feat: implemented read

--- src/lib-header/fat32.h
+++ src/lib-header/fat32.h
@@ -203,7 +203,7 @@ void read_clusters(void *ptr, uint32_t cluster_number, uint8_t cluster_count);
  *                ext is unused,
  *                parent_cluster_number is target directory table to read,
  *                buffer_size must be exactly sizeof(struct FAT32DirectoryTable)
- * @return Error code: 0 success - 1 not a folder - 2 not found - -1 unknown
+ * @return Error code: 0 success - 1 not a file - 2 not enough buffer - 3 not found - -1 unknown
  */
 int8_t read_directory(struct FAT32DriverRequest request);
 
