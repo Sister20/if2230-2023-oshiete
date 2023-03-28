d15c76abc845c33df8a1002455ecfe531d6bee78
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2023-03-28 23:55:46 +0700

    fix: cmos and use undelete

--- src/cmos.c
+++ src/cmos.c
@@ -18,4 +18,19 @@ void cmos_read_rtc(struct time *t)
     t->year = in(CMOS_DATA_PORT);
     out(CMOS_ADDRESS_PORT, 0x32);
     t->year |= in(CMOS_DATA_PORT) << 8;
+
+    // Convert BCD values to binary
+    t->second = ((t->second & 0xF0) >> 4) * 10 + (t->second & 0x0F);
+    t->minute = ((t->minute & 0xF0) >> 4) * 10 + (t->minute & 0x0F);
+
+    t->hour = ((t->hour & 0xF0) >> 4) * 10 + (t->hour & 0x0F);
+    if ((t->hour & 0x80) != 0) // if the high bit is set
+    {
+        // convert to 24-hour format by subtracting 12 from the hours
+        t->hour = ((t->hour & 0x7F) + 12) % 24;
+    }
+    
+    t->day = ((t->day & 0xF0) >> 4) * 10 + (t->day & 0x0F);
+    t->month = ((t->month & 0xF0) >> 4) * 10 + (t->month & 0x0F);
+    t->year = ((t->year & 0xF0) >> 4) * 10 + (t->year & 0x0F);
 }
