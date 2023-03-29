e78836292c4e02fffc72e6df5b26885fb1b247e5
Altair1618 <103034064+Altair1618@users.noreply.github.com>
2023-03-29 09:42:26 +0700

    feat: support capslock and shift for capital

--- src/lib-header/keyboard.h
+++ src/lib-header/keyboard.h
@@ -35,6 +35,8 @@ struct KeyboardDriverState {
     bool    keyboard_input_on;
     uint8_t buffer_index;
     char    keyboard_buffer[KEYBOARD_BUFFER_SIZE];
+    bool    shift_pressed;
+    bool    capslock_activated;
 } __attribute((packed));
 
 /* -- Driver Interfaces -- */
