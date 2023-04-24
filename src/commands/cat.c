32e6ddcff5f2b6ab900047585026fde6e979c9c0
Jeffrey Chow <chowprofessional@gmail.com>
2023-04-24 13:45:17 +0700

    improve cat

--- src/commands/cat.c
+++ src/commands/cat.c
@@ -8,13 +8,21 @@ void cat(struct CurrentWorkingDirectory cwd, char *file_path)
 
     // SEPERATE FILE NAME INTO NAME AND EXT
     const char* delim = ".";
+    char* name;
+    char* ext;
 
-    char* name = strtok(file_name, delim);
-    char* ext = strtok(NULL, delim);
+    name = strtok(file_name, delim);
+    ext = strtok(NULL, delim);
+
+    // IF NO EXTENSION, REPLACE EXTENSION WITH \0
+    if (ext == NULL){
+        memcpy(ext, "\0\0\0", 3);
+    }
 
     // CHECK IF NAME AND EXT OUTSIDE CONSTRAINTS
     if (strlen(name) > 8 || strlen(ext) > 3) {
         retcode = 3;
+        puts("Error: File name and extension out of constraints", VGA_COLOR_RED);
         return;
     }
 
@@ -30,9 +38,10 @@ void cat(struct CurrentWorkingDirectory cwd, char *file_path)
     };
 
     // COPY NAME AND EXT TO req
-    memcpy(req.name, name, 8);
-    memcpy(req.ext, ext, 3);
+    memcpy(req.name, name, strlen(name));
+    memcpy(req.ext, ext, strlen(ext));
 
+    // SYSCALL TO READ FILEs
     syscall(0, (uint32_t)&req, (uint32_t)&retcode, (uint32_t)&found_cluster_number);
 
     // IF SYSCALL SUCCEDEED
@@ -45,8 +54,16 @@ void cat(struct CurrentWorkingDirectory cwd, char *file_path)
             for (int j = 0; j < CLUSTER_SIZE; j++) {
                 char c = (char) item.buf[j];
                 char str[2] = {c, '\0'};
-                puts(str, VGA_COLOR_RED);
+                puts(str, VGA_COLOR_WHITE);
             }
         }     
+    } else if (retcode == 1){
+        puts("Error: Not a file", VGA_COLOR_RED);
+    } else if (retcode == 2){
+        puts("Error: Not enough buffer", VGA_COLOR_RED);
+    } else if (retcode == 3) {
+        puts("Error: File not found", VGA_COLOR_RED);
+    } else {
+        puts("Error: Unknown", VGA_COLOR_RED);
     }
 }
