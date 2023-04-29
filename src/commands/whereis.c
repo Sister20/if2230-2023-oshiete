bd836531ef52c75583fc0ef48d123f67e1e10f18
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2023-04-29 22:32:12 +0700

    change whereis

--- src/commands/whereis.c
+++ src/commands/whereis.c
@@ -2,8 +2,13 @@
 
 void whereis(char *file_name)
 {
+    char name[8] = "\0";
+    char ext[3] = "\0";
+
+    separate_filename(file_name, name, ext);
+
     // Initialize Output
-    puts(file_name, VGA_COLOR_WHITE);
+    puts(name, VGA_COLOR_WHITE);
     puts(": ", VGA_COLOR_WHITE);
 
     // Initialize Current Working Directory
@@ -36,38 +41,40 @@ void whereis(char *file_name)
                 cwd.top++;
                 strcpy(cwd.dir_names[cwd.top], dir_table.table[i].name);
                 cwd.clusters_stack[cwd.top] = (dir_table.table[i].cluster_high << 16 | dir_table.table[i].cluster_low);
-                searchUtil(&cwd, file_name);
+                searchUtil(&cwd, name, ext);
                 cwd.top--;
             }
             else
             {
                 // If File then Check If It Has The Same Name
-                if (!memcmp(dir_table.table[i].name, file_name, strlen(file_name)))
+                if (!memcmp(dir_table.table[i].name, name, strlen(name)))
                 {
-                    // If Same Name then Print The Path
-                    for (int j = 0; j <= cwd.top; j++)
-                    {
-                        puts(cwd.dir_names[j], VGA_COLOR_WHITE);
-                        puts("/", VGA_COLOR_WHITE);
-                    }
-                    // Print The File Name
-                    puts(file_name, VGA_COLOR_WHITE);
+                    if (ext[0] == '\0' || (ext[0] != '\0' && !memcmp(dir_table.table[i].ext, ext, strlen(ext)))){
+                        // If Same Name then Print The Path
+                        for (int j = 0; j <= cwd.top; j++)
+                        {
+                            puts(cwd.dir_names[j], VGA_COLOR_WHITE);
+                            puts("/", VGA_COLOR_WHITE);
+                        }
+                        // Print The File Name
+                        puts(name, VGA_COLOR_WHITE);
 
-                    // Print Extension if Exists
-                    if (dir_table.table[i].ext[0] != '\0')
-                    {
-                        puts(".", VGA_COLOR_WHITE);
-                        puts(dir_table.table[i].ext, VGA_COLOR_WHITE);
-                    }
+                        // Print Extension if Exists
+                        if (dir_table.table[i].ext[0] != '\0')
+                        {
+                            puts(".", VGA_COLOR_WHITE);
+                            puts(dir_table.table[i].ext, VGA_COLOR_WHITE);
+                        }
 
-                    puts("  ", VGA_COLOR_WHITE);
+                        puts("  ", VGA_COLOR_WHITE);
+                    }
                 }
             }
         }
     }
 }
 
-void searchUtil(struct CurrentWorkingDirectory *cwd, char *file_name)
+void searchUtil(struct CurrentWorkingDirectory *cwd, char *name, char* ext)
 {
     // Read Current Directory
     int8_t retcode = -1;
@@ -93,31 +100,33 @@ void searchUtil(struct CurrentWorkingDirectory *cwd, char *file_name)
                 cwd->top++;
                 strcpy(cwd->dir_names[cwd->top], dir_table.table[i].name);
                 cwd->clusters_stack[cwd->top] = (dir_table.table[i].cluster_high << 16 | dir_table.table[i].cluster_low);
-                searchUtil(cwd, file_name);
+                searchUtil(cwd, name, ext);
                 cwd->top--;
             }
             else
             {
                 // If File then Check If It Has The Same Name
-                if (!memcmp(dir_table.table[i].name, file_name, strlen(file_name)))
+                if (!memcmp(dir_table.table[i].name, name, strlen(name)))
                 {
-                    // If Same Name then Print The Path
-                    for (int j = 0; j <= cwd->top; j++)
-                    {
-                        puts(cwd->dir_names[j], VGA_COLOR_WHITE);
-                        puts("/", VGA_COLOR_WHITE);
-                    }
-                    // Print The File Name
-                    puts(file_name, VGA_COLOR_WHITE);
+                    if (ext[0] == '\0' || (ext[0] != '\0' && !memcmp(dir_table.table[i].ext, ext, strlen(ext)))){
+                        // If Same Name then Print The Path
+                        for (int j = 0; j <= cwd->top; j++)
+                        {
+                            puts(cwd->dir_names[j], VGA_COLOR_WHITE);
+                            puts("/", VGA_COLOR_WHITE);
+                        }
+                        // Print The File Name
+                        puts(name, VGA_COLOR_WHITE);
 
-                    // Print Extension if Exists
-                    if (dir_table.table[i].ext[0] != '\0')
-                    {
-                        puts(".", VGA_COLOR_WHITE);
-                        puts(dir_table.table[i].ext, VGA_COLOR_WHITE);
-                    }
+                        // Print Extension if Exists
+                        if (dir_table.table[i].ext[0] != '\0')
+                        {
+                            puts(".", VGA_COLOR_WHITE);
+                            puts(dir_table.table[i].ext, VGA_COLOR_WHITE);
+                        }
 
-                    puts("  ", VGA_COLOR_WHITE);
+                        puts("  ", VGA_COLOR_WHITE);
+                    }    
                 }
             }
         }
